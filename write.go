bf73bef1bbdd07b438927a75f44f0c139fc478a6
GabielFemi <gabrielfemi799@gmail.com>
2020-05-12 10:09:03 +0200

    feature: add write to a file option
    Took 10 minutes

--- write.go
+++ write.go
@@ -1,2 +1,11 @@
 package go_filer
 
+import (
+	"io/ioutil"
+)
+
+func WriteFile(message string, filePath string) bool{
+	messageToByte := []byte (message)
+	_ = ioutil.WriteFile(filePath, messageToByte, 0644)
+	return true
+}
